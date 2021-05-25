ad1bbab561ad89717b30d783833b3655435af913
Kalleby <kalleby.santos2k@gmail.com>
2021-05-25 11:45:44 +0100

    📦 Creating App layout wrapper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Header from '../components/Header';
 
 export default function Home() {
   return (
     <div>
-      <Header />
+      <h1>Hello world 🔥</h1>
     </div>
   );
 }

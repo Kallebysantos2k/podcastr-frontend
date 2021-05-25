472234dc8c1cec398d44144a3164eafb9019871b
Kalleby <kalleby.santos2k@gmail.com>
2021-05-25 11:36:47 +0100

    📦 Creating Header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
+import React from 'react';
+import Header from '../components/Header';
+
 export default function Home() {
   return (
-    <h1>Hello World 😃</h1>
+    <div>
+      <Header />
+    </div>
   );
 }

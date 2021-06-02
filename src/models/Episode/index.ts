29e75d1dbeda5681d9393829e0c1c28162a8bdf3
Kalleby <kalleby.santos2k@gmail.com>
2021-06-02 09:33:21 +0100

    📦 NEW: Episode model interface

---
+++ src/models/Episode/index.ts
@@ -0,0 +1,10 @@
+export interface Episode {
+  id: string,
+  name: string,
+  members: string,
+  duration: number,
+  description: string,
+  publishedAt: string,
+  audio: string,
+  thumbnail: string,
+}

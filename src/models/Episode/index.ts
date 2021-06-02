4bbe6cdde23daa5c3aceef7394e89435dfecfe28
Kalleby <kalleby.santos2k@gmail.com>
2021-06-02 09:33:48 +0100

    📦 NEW: Episode Parser function

--- src/models/Episode/index.ts
+++ src/models/Episode/index.ts
@@ -1,3 +1,6 @@
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 export interface Episode {
   id: string,
   name: string,
@@ -7,4 +10,17 @@ export interface Episode {
   publishedAt: string,
   audio: string,
   thumbnail: string,
+}
+
+export function parseToEpisode(data: any): Episode {
+  return {
+    id: data.id,
+    name: data.name,
+    members: data.members,
+    duration: Number(data.duration),
+    description: data.description,
+    publishedAt: format(parseISO(data.publishedAt), 'd MMM yy', { locale: ptBR }),
+    audio: data.fileUrl,
+    thumbnail: data.thumbnailUrl,
+  };
 }

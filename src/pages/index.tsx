18f920a2b6ac5c8fb5b45e3c63dd373f147bf97b
Kalleby <kalleby.santos2k@gmail.com>
2021-06-02 09:36:21 +0100

    👌 IMPROVE: Applying types to HomeProps and SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,28 @@
-import axios from 'axios';
 import React from 'react';
+import { GetStaticProps } from 'next';
+import api from '../services/api';
+import { Episode, parseToEpisode } from '../models/Episode';
 
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2MjI1MzY1MDcsImV4cCI6MTYyMjU0MDEwNywicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJuYW1lIjoia2FsbGVieUBlbWFpbC5jb20ifQ.bucaY2SawXey3DQIZB-zzuWa8aIEJAV130FNwbMocEWHBUsXmosK-toHZfM27H_Rwrl0d-VstD-cdPB98QxNtMPexANrmkl5HCkBdwlRS8fSxBkmQ9UyJYbwZV8aaWlZBNz8SxxB1KHCxWIQEjve_rOjxRVj38YHubL9VD8COObm3SUk_HPtfAOi58OS8h0QGHlFf7lO_PyGL0-fFo5lNUJyQPO28Wm29fpjgAN6JwA2eazlJptbn7i49gBT37TFPb67GX5fquD_oD-joBGHjVBYaeFATRWkSNRZrmJhR9oPoqNadIJGwvhFzn9nPQra7Y6TqmCIsfZ3cYRhccKBfQ';
-
-export default function Home(props) {
-  console.log(props.episodes);
-  const episodes = JSON.stringify(props.episodes);
-  return (
-    <div>
-      <h1>Episodes🔥</h1>
-      <p>{episodes}</p>
-    </div>
-  );
+interface HomeProps {
+  episodes: Episode[],
 }
 
-export async function getStaticProps() {
-  const response = await axios.get('http://localhost:8000/podcast/', {
-    headers: { Authorization: `Bearer ${token}` },
-  });
-
-  const data = await response.data;
-  console.log(data);
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const { data } = await api.get('podcast/');
 
   return {
     props: {
-      episodes: data,
+      episodes: data.map(parseToEpisode),
     },
     revalidate: 60 * 2,
   };
+};
+
+export default function Home({ episodes }: HomeProps) {
+  return (
+    <div>
+      <h1>Episodes🔥</h1>
+      <p>{JSON.stringify(episodes)}</p>
+    </div>
+  );
 }

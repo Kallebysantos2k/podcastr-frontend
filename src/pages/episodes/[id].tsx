73c98ce37e931e6cf508babe1a27960e81fda2af
Kalleby <kalleby.santos2k@gmail.com>
2021-06-04 11:41:52 +0100

    👌 IMPROVE: Episode Page, Layout style

--- src/pages/episodes/[id].tsx
+++ src/pages/episodes/[id].tsx
@@ -1,6 +1,9 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Link from 'next/link';
+import Image from 'next/image';
 import { Episode, parseToEpisode } from '../../models/Episode';
 import api from '../../services/api';
+import styles from './styles.module.scss';
 
 interface EpisodeProps {
   episode: Episode,
@@ -24,8 +27,40 @@ export const getStaticProps: GetStaticProps<EpisodeProps> = async (ctx) => {
 
 export default function EpisodePage({ episode }: EpisodeProps) {
   return (
-    <div>
-      <h2>{episode.name}</h2>
+    <div className={styles.episodeContainer}>
+      <div className={styles.thumbnailContainer}>
+        <Link href="/">
+          <button type="button">
+            <img src="/arrow-left.svg" alt="Voltar" />
+          </button>
+        </Link>
+
+        <Image
+          width={700}
+          height={160}
+          objectFit="cover"
+          src={episode.thumbnail}
+        />
+
+        <button type="button">
+          <img src="/play.svg" alt="Reproduzir episodio" />
+        </button>
+      </div>
+
+      <header>
+        <h1>{episode.name}</h1>
+        <div className={styles.episodeInfo}>
+          <span>{episode.members}</span>
+          <div>
+            <span>{episode.publishedAt}</span>
+            <span>{episode.timeString}</span>
+          </div>
+        </div>
+      </header>
+
+      <div className={styles.description}>
+        <p>{episode.description}</p>
+      </div>
     </div>
   );
 }

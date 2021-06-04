24b446fe7bff5ed3df6dfdc31c9130ff7281fd3f
Kalleby <kalleby.santos2k@gmail.com>
2021-06-04 10:37:48 +0100

    📦 NEW: Episode Page with SSG

--- src/pages/episodes/[id].tsx
+++ src/pages/episodes/[id].tsx
@@ -1,11 +1,31 @@
-import { useRouter } from 'next/router';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { Episode, parseToEpisode } from '../../models/Episode';
+import api from '../../services/api';
 
-export default function Episode() {
-  const router = useRouter();
+interface EpisodeProps {
+  episode: Episode,
+}
 
+export const getStaticPaths: GetStaticPaths = async () => ({
+  paths: [],
+  fallback: 'blocking',
+});
+
+export const getStaticProps: GetStaticProps<EpisodeProps> = async (ctx) => {
+  const { id } = ctx.params;
+  const { data } = await api.get(`podcast/${id}`);
+
+  return {
+    props: {
+      episode: parseToEpisode(data),
+    },
+  };
+};
+
+export default function EpisodePage({ episode }: EpisodeProps) {
   return (
     <div>
-      <h2>{router.query.id}</h2>
+      <h2>{episode.name}</h2>
     </div>
   );
 }

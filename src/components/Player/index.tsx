2e5b5cd541dae386590b15461cb059de75d98896
Kalleby <kalleby.santos2k@gmail.com>
2021-06-07 11:52:36 +0100

    👌 Showing custom slider on Play episode

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,4 +1,8 @@
 import { useContext } from 'react';
+
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+
 import Image from 'next/image';
 import { PlayerContext } from '../../contexts/PlayerContext';
 import styles from './styles.module.scss';
@@ -36,12 +40,24 @@ export default function Player() {
           )
       }
 
-      <footer className={styles.empty}>
+      <footer className={!episode && styles.empty}>
         <div className={styles.progress}>
           <span>00:00</span>
 
           <div className={styles.slider}>
-            <div className={styles.emptySlider} />
+            {
+              episode
+                ? (
+                  <Slider
+                    trackStyle={{ backgroundColor: '#04d361' }}
+                    railStyle={{ backgroundColor: '#9f75ff' }}
+                    handleStyle={{ borderColor: '#9f75ff' }}
+                  />
+                )
+                : (
+                  <div className={styles.emptySlider} />
+                )
+            }
           </div>
 
           <span>00:00</span>

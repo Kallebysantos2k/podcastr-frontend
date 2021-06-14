228979770093709cc485edf650d90eb63ec7299b
Kalleby <kalleby.santos2k@gmail.com>
2021-06-14 18:46:13 +0100

    Setting auth page to default index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,127 +1,49 @@
-import React from 'react';
-import { GetStaticPropsResult } from 'next';
-import Head from 'next/head';
-import Link from 'next/link';
-import Image from 'next/image';
-import api from '../services/api';
-import styles from '../styles/home.module.scss';
-import { Episode, parseToEpisode } from '../models/Episode';
-import { usePlayer } from '../contexts/PlayerContext';
+import React, { useState } from 'react';
+import { SignIn, SignUp } from '../components/AuthForm';
+import styles from '../styles/auth.module.scss';
 
-interface HomeProps {
-  latestEpisodes: [Episode],
-  allEpisodes: [Episode],
-}
+export default function Auth() {
+  const [isSignIn, setIsSignIn] = useState(false);
 
-export async function getStaticProps(): Promise<GetStaticPropsResult<HomeProps>> {
-  const { data } = await api.get('podcast/');
-
-  const parsedData = data.map(parseToEpisode);
-  const episodes = parsedData.sort((a, b) => b.id - a.id);
-
-  return {
-    props: {
-      latestEpisodes: episodes.slice(0, 2),
-      allEpisodes: episodes.slice(2, episodes.length),
-    },
-    revalidate: 60 * 2,
-  };
-}
-
-export default function Home({ allEpisodes, latestEpisodes }: HomeProps) {
-  const { playList } = usePlayer();
-
-  const episodesList = [...latestEpisodes, ...allEpisodes];
+  function toggleSignOperation() {
+    setIsSignIn(!isSignIn);
+  }
 
   return (
-    <div className={styles.homeContainer}>
-      <Head>
-        <title>Home | Podcastr</title>
-      </Head>
+    <div className={styles.authContainer}>
 
-      <section className={styles.latestEpisodesContainer}>
-        <h2>Últimos lançamentos</h2>
+      <aside>
+        <h1>Podcastr</h1>
+        <p>O melhor para você ouvir, sempre</p>
+      </aside>
 
-        <ul>
-          { latestEpisodes.map((episode, index) => (
-            <li key={episode.id}>
+      <main>
+        {
+          isSignIn ? (<SignIn />) : (<SignUp />)
+        }
 
-              <div className={styles.latestEpisodeThumbnail}>
-                <Image
-                  width={192}
-                  height={192}
-                  src={episode.thumbnail}
-                  alt={episode.name}
-                />
-              </div>
-
-              <div className={styles.episodeDetails}>
-                <Link href={`/episodes/${episode.id}`}>
-                  <a>{episode.name}</a>
-                </Link>
-
-                <p>{episode.members}</p>
-                <span>{episode.publishedAt}</span>
-                <span>{episode.timeString}</span>
-              </div>
-
-              <button type="button" onClick={() => playList(episodesList, index)}>
-                <img src="/play-green.svg" alt="Reproduzir episodio" />
-              </button>
-            </li>
-          ))}
-        </ul>
-      </section>
-
-      <section className={styles.allEpisodesContainer}>
-        <h2>Todos episodios</h2>
-
-        <table cellSpacing={0}>
-          <thead>
-            <tr>
-              <th />
-              <th>Podcast</th>
-              <th>Participantes</th>
-              <th>Data</th>
-              <th>Duração</th>
-              <th />
-            </tr>
-          </thead>
-
-          <tbody>
-            {allEpisodes.map((episode, index) => (
-              <tr key={episode.id}>
-                <td className={styles.episodeThumbnail}>
-                  <Image
-                    width={120}
-                    height={120}
-                    src={episode.thumbnail}
-                    alt={episode.name}
-                  />
-                </td>
-
-                <td className={styles.episodeInfo}>
-                  <Link href={`/episodes/${episode.id}`}>
-                    <a>{episode.name}</a>
-                  </Link>
-                </td>
-
-                <td className={styles.episodeInfo}>
-                  {episode.members}
-                </td>
-
-                <td>{episode.publishedAt}</td>
-                <td>{episode.timeString}</td>
-                <td>
-                  <button type="button" onClick={() => playList(episodesList, index + latestEpisodes.length)}>
-                    <img src="/play-green.svg" alt="Reproduzir episodio" />
+        <footer>
+          {
+            isSignIn
+              ? (
+                <>
+                  <p>Ainda não possui uma conta?</p>
+                  <button type="button" onClick={toggleSignOperation}>
+                    Fazer cadastro!
                   </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </section>
+                </>
+              )
+              : (
+                <>
+                  <p>Já possui uma conta?</p>
+                  <button type="button" onClick={toggleSignOperation}>
+                    Fazer login!
+                  </button>
+                </>
+              )
+          }
+        </footer>
+      </main>
     </div>
   );
 }

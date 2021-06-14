5b04f5ad879c1a54c7bf0f286a03a72444d5a0fb
Kalleby <kalleby.santos2k@gmail.com>
2021-06-14 09:10:05 +0100

    📦 NEW: Handle email input from sign in

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -1,5 +1,34 @@
+import React, { ChangeEvent, FormEvent, useState } from 'react';
+import InputArea from '../InputArea';
+import styles from './styles.module.scss';
+
 export default function SignIn() {
+  const [email, setEmail] = useState('');
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log(email);
+  }
+
+  function handleEmailInputChange(event: ChangeEvent<HTMLInputElement>) {
+    const { value } = event.target;
+
+    setEmail(value);
+  }
+
   return (
-    <div>SignIn</div>
+    <form
+      className={styles.formContainer}
+      onSubmit={(e) => handleSubmit(e)}
+    >
+      <InputArea
+        name="email"
+        label="Seu email:"
+        type="email"
+        value={email}
+        onChange={(e) => handleEmailInputChange(e)}
+      />
+
+      <button type="submit">ok</button>
+    </form>
   );
 }

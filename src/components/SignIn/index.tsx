232583d13f51ba5f2837bc002b92d93dba26a4d2
Kalleby <kalleby.santos2k@gmail.com>
2021-06-14 09:50:32 +0100

    📦 NEW: Handling Password input from sign in

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -4,15 +4,23 @@ import styles from './styles.module.scss';
 
 export default function SignIn() {
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log(email);
+    console.log(email, password);
   }
 
   function handleEmailInputChange(event: ChangeEvent<HTMLInputElement>) {
     const { value } = event.target;
 
     setEmail(value);
+  }
+
+  function handlePasswordInputChange(event: ChangeEvent<HTMLInputElement>) {
+    const { value } = event.target;
+
+    setPassword(value);
   }
 
   return (
@@ -26,6 +34,14 @@ export default function SignIn() {
         type="email"
         value={email}
         onChange={(e) => handleEmailInputChange(e)}
+      />
+
+      <InputArea
+        name="password"
+        label="Sua senha:"
+        type="password"
+        value={password}
+        onChange={(e) => handlePasswordInputChange(e)}
       />
 
       <button type="submit">ok</button>

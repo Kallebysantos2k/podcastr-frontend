c82e9eb5c186be2ae0359e3c4f4f8c1e5413b4e3
Kalleby <kalleby.santos2k@gmail.com>
2021-06-16 09:49:24 +0100

    👌 IMPROVE: Dashboard isAdmin validation

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,3 +1,37 @@
+import { GetServerSideProps } from 'next';
+import { parseCookies } from 'nookies';
+import { User } from '../../models/User';
+import api from '../../services/api';
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { 'podcastr.token': token } = parseCookies(ctx);
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  api.defaults.headers.Authorization = `Bearer ${token}`;
+
+  const { data } = await api.get('/user') as { data: User };
+  const isAdmin = !!data?.roles.filter((role) => role === 'ROLE_ADMIN')[0];
+
+  if (!isAdmin) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  return { props: {} };
+};
+
 export default function Dashboard() {
   return (
     <div>

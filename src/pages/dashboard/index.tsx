b23fcdc340117c722bdee50059f0974963759baf
Kalleby <kalleby.santos2k@gmail.com>
2021-06-16 10:37:04 +0100

    📦 NEW: Display all episodes on list

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,8 @@
 import { GetServerSideProps } from 'next';
 import { parseCookies } from 'nookies';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
 import { Episode, parseToEpisode } from '../../models/Episode';
 import { User } from '../../models/User';
 import api from '../../services/api';
@@ -46,9 +49,55 @@ export const getServerSideProps: GetServerSideProps<DashboardProps> = async (ctx
 };
 
 export default function Dashboard({ allEpisodes }: DashboardProps) {
+  const [episodes, setEpisodes] = useState(allEpisodes);
+
   return (
     <div>
-      {JSON.stringify(allEpisodes)}
+      <section>
+        <h2>Todos episodios</h2>
+
+        <table cellSpacing={0}>
+          <thead>
+            <tr>
+              <th />
+              <th>Id</th>
+              <th>Podcast</th>
+              <th>Data</th>
+              <th />
+            </tr>
+          </thead>
+
+          <tbody>
+            {episodes.map((episode) => (
+              <tr key={episode.id}>
+                <td>
+                  <Image
+                    width={120}
+                    height={120}
+                    src={episode.thumbnail}
+                    alt={episode.name}
+                  />
+                </td>
+
+                <td>{episode.id}</td>
+
+                <td>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.name}</a>
+                  </Link>
+                </td>
+
+                <td>{episode.publishedAt}</td>
+                <td>
+                  <button type="button">
+                    <img src="/play-green.svg" alt="Reproduzir episodio" />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
     </div>
   );
 }

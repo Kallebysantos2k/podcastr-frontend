9cbf7792f7459236fa95dc272a140b356c91c1e3
Kalleby <kalleby.santos2k@gmail.com>
2021-06-21 09:40:19 +0100

    👌 IMPROVE: UpdateEpisode handling form

--- src/pages/dashboard/updateEpisode/[id].tsx
+++ src/pages/dashboard/updateEpisode/[id].tsx
@@ -1,23 +1,86 @@
 import { GetServerSideProps } from 'next';
+import React from 'react';
+import Link from 'next/link';
+import { MdArrowBack, MdArrowForward } from 'react-icons/md';
+import { useForm } from 'react-hook-form';
+import styles from './styles.module.scss';
+import api from '../../../services/api';
+import { Episode, parseToEpisode } from '../../../models/Episode';
+import InputArea from '../../../components/InputArea';
 
 interface UpdateEpisodeProps {
-  id: number;
+  episode: Episode
 }
 
 export const getServerSideProps: GetServerSideProps<UpdateEpisodeProps> = async (ctx) => {
   const { id } = ctx.params;
 
+  const { data } = await api.get(`podcast/${id}`);
+
   return {
     props: {
-      id: Number(id),
+      episode: parseToEpisode(data),
     },
   };
 };
 
-export default function UpdateEpisode({ id }: UpdateEpisodeProps) {
+export default function UpdateEpisode({ episode }: UpdateEpisodeProps) {
+  const { register, handleSubmit, control } = useForm();
+
+  function handleUpdateEpisode(data) {
+    console.log(data);
+  }
+
   return (
-    <div>
-      <h2>Update Episode {id}</h2>
+    <div className={styles.updateEpisodeContainer}>
+      <header>
+        <Link href="/dashboard">
+          <a>
+            <MdArrowBack />
+            <span>Voltar</span>
+          </a>
+        </Link>
+        <h2>Editar episódio {episode.name}</h2>
+      </header>
+
+      <form onSubmit={handleSubmit(handleUpdateEpisode)}>
+        <InputArea
+          name="name"
+          type="text"
+          label="Titulo"
+          control={control}
+          defaultValue={episode.name}
+          otherProps={register('name')}
+          required
+        />
+
+        <InputArea
+          name="description"
+          type="text"
+          label="Descrição"
+          control={control}
+          defaultValue={episode.description}
+          otherProps={register('description')}
+          required
+        />
+
+        <InputArea
+          name="members"
+          type="text"
+          label="Participantes"
+          control={control}
+          defaultValue={episode.members}
+          otherProps={register('members')}
+          required
+        />
+
+        <footer>
+          <button type="submit">
+            <span>Confirmar</span>
+            <MdArrowForward />
+          </button>
+        </footer>
+      </form>
     </div>
   );
 }
